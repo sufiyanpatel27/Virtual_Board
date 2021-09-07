97e036eec1ac4698c8cbfa5a213d786adf68fd99
sufiyan <sp3596894@gmail.com>
2021-09-07 16:43:09 +0530

    drawing with opencv toolkit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     while cap.isOpened():
         ret, frame = cap.read()
 
-        image = imutils.resize(frame, width=1200)
+        image = imutils.resize(frame, width=1200, height=800)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = holistic.process(image)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -26,12 +26,26 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                                   )
 
         # 3. Left Hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+        '''mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                                   mp_drawing.DrawingSpec(
                                       color=(121, 22, 76), thickness=2, circle_radius=4),
                                   mp_drawing.DrawingSpec(
                                       color=(121, 44, 250), thickness=2, circle_radius=2)
-                                  )
+                                  )'''
+        # here
+        # print(results.left_hand_landmarks)
+        try:
+            x = []
+            y = []
+            for data_point in results.left_hand_landmarks.landmark:
+                x.append(data_point.x)
+                y.append(data_point.y)
+
+                for i in range(len(x)):
+                    cv2.circle(
+                        image, (int(x[i]*1200), int(y[i]*900)), 5, (0, 0, 255), 10)
+        except:
+            print("not visible")
 
         cv2.imshow('Raw Webcam Feed', image)
         key = cv2.waitKey(1) & 0xff
